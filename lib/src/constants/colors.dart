92b6420f06728eaabf1cfaa6b33649d8004a7530
ntntoan1@gmail.com <ntntoan1@gmail.com>
2024-02-21 21:19:47 +0700

    Create colors and size variable for CrossWidget, CircleWidget, and border

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -1,7 +1,14 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 // === Light Colors ===
 const lightColor700 = Color(0xFFFAFAFA);
 
 // === Dark Colors ===
 const darkColor500 = Color(0xFF242424);
+
+// === Widget Colors ===
+const crossWidgetColor = Colors.blueAccent;
+const circleWidgetColor = Colors.redAccent;
+const borderColor = Colors.grey;
---
+++ lib/src/constants/sizes.dart
@@ -0,0 +1,2 @@
+const double widgetStrokeWidth = 8.0;
+const double borderWidth = 1.6;
