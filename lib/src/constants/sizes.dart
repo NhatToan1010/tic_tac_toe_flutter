5b8a8a9aa85c42c11cf8b6b5c0b2c750ddbb63c1
ntntoan1@gmail.com <ntntoan1@gmail.com>
2024-03-05 20:32:57 +0700

    Create iconButtonWidth and iconButtonHeight

--- lib/src/constants/sizes.dart
+++ lib/src/constants/sizes.dart
@@ -7,6 +7,8 @@ const double utilWidgetHeight = 54.0;
 // Button
 const double buttonWidth = 190.0;
 const double buttonHeight = 120.0;
+const double iconButtonWidth = 56.0;
+const double iconButtonHeight = 56.0;
 
 // === Text Sizes ===
 const double headingLargeSize = 56.0;
