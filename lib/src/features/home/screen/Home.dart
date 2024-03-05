fa0dd7c59306e0d1943938fd9a2e4aa00bb5911d
ntntoan1@gmail.com <ntntoan1@gmail.com>
2024-03-05 15:25:03 +0700

    Create Home Screen GameModeSelectionWidget()

--- lib/src/features/home/screen/Home.dart
+++ lib/src/features/home/screen/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/src/constants/colors.dart';
+import 'package:tic_tac_toe/src/features/home/screen/widgets/game_mode_selection.dart';
 import 'package:tic_tac_toe/src/features/home/screen/widgets/title.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,15 +9,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final txtTheme = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
+          width: size.width,
+          height: size.height,
           color: primaryBackgroundColor,
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TitleWidget(txtTheme: txtTheme),
+              GameModeSelectionWidget(txtTheme: txtTheme)
             ],
           ),
         ),
@@ -24,3 +31,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
