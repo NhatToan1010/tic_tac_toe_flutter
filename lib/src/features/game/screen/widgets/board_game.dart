3f3c10d07dea429221c41c041bd2f129eef334d2
ntntoan1@gmail.com <ntntoan1@gmail.com>
2024-03-23 08:24:57 +0700

    Add GameController

--- lib/src/features/game/screen/widgets/board_game.dart
+++ lib/src/features/game/screen/widgets/board_game.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tic_tac_toe/src/constants/colors.dart';
+import 'package:tic_tac_toe/src/features/game/controller/controller.dart';
+import 'package:tic_tac_toe/src/utils/mechanisms/game_mechanism.dart';
+import 'package:tic_tac_toe/src/utils/widgets/circle_widget.dart';
+import 'package:tic_tac_toe/src/utils/widgets/cross_widget.dart';
 
-class BoardGameWidget extends StatelessWidget {
+class BoardGameWidget extends GetWidget<GameController> {
   const BoardGameWidget({
     super.key,
     required this.size,
@@ -17,18 +22,68 @@ class BoardGameWidget extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           _listenBoardData(),
+          _listenAiPlaying(),
+          _listenGameResult(),
         ],
       ),
     );
   }
 
+  Widget _listenGameResult() {
+    return Obx(() => IgnorePointer(
+          ignoring: controller.gameResult.value == 0,
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            color: controller.gameResult.value != 0
+                ? Colors.grey[200]
+                : Colors.transparent,
+            child: _gameResultAnnounce(),
+          ),
+        ));
+  }
+
+  Widget? _gameResultAnnounce() {
+    final _gameResultStatus = controller.gameResultSatus;
+    if (_gameResultStatus != null) {
+      return Center(
+        child: Text(
+          _gameResultStatus,
+          style: const TextStyle(
+              fontSize: 30.0, color: Colors.grey, fontWeight: FontWeight.bold),
+        ),
+      );
+    }
+    return null;
+  }
+
+  // If Multiplayer mode => Ignore Ai
+  Widget _listenAiPlaying() {
+    return Obx(() => IgnorePointer(
+          ignoring: !controller.isAiPlaying.value,
+          child: Container(
+            color: Colors.transparent,
+          ),
+        ));
+  }
+
   Widget _listenBoardData() {
-    return GridView.count(
-      crossAxisCount: 3,
-      shrinkWrap: true,
-      childAspectRatio: 1.0,
-      physics: const NeverScrollableScrollPhysics(),
-      children: List.generate(9, (index) => const _FieldWidget()),
+    return Obx(
+      () => GridView.count(
+        crossAxisCount: 3,
+        shrinkWrap: true,
+        childAspectRatio: 1.0,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+            controller.boardGame.length,
+            (index) => _FieldWidget(
+                  onTap: (value) {
+                    controller.move(value);
+                  },
+                  isEnabled: controller.isEnabled(index),
+                  index: index,
+                  playerId: controller.getDataAt(index),
+                )),
+      ),
     );
   }
 }
@@ -36,12 +91,21 @@ class BoardGameWidget extends StatelessWidget {
 class _FieldWidget extends StatelessWidget {
   const _FieldWidget({
     Key? key,
+    required this.isEnabled,
+    required this.index,
+    required this.playerId,
+    required this.onTap,
   }) : super(key: key);
+
+  final bool isEnabled;
+  final int index;
+  final int playerId;
+  final ValueChanged<int> onTap;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: isEnabled ? () => onTap(index) : null,
       child: DecoratedBox(
         decoration: BoxDecoration(
           color: Colors.amberAccent.withAlpha(20),
@@ -51,11 +115,22 @@ class _FieldWidget extends StatelessWidget {
           decoration: BoxDecoration(
               color: primaryGrey.withOpacity(0.5),
               borderRadius: const BorderRadius.all(Radius.circular(10.0))),
-          child: const Padding(
-            padding: EdgeInsets.all(32.0),
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: _getPlayer(playerId),
           ),
         ),
       ),
     );
+  }
+
+  Widget? _getPlayer(int playerId) {
+    switch (playerId) {
+      case GameMechanism.player1:
+        return const CrossWidget();
+      case GameMechanism.player2:
+        return const CircleWidget();
+    }
+    return null;
   }
 }
