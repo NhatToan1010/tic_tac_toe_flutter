28ce6ce27dced44afc94c343f65320734771b4b1
ntntoan1@gmail.com <ntntoan1@gmail.com>
2024-03-23 08:25:53 +0700

    Create GameBiding for isMultiplayer

--- lib/src/features/game/screen/game.dart
+++ lib/src/features/game/screen/game.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:tic_tac_toe/src/constants/colors.dart';
+import 'package:tic_tac_toe/src/features/game/controller/controller.dart';
 import 'package:tic_tac_toe/src/features/game/screen/widgets/board_game.dart';
+import 'package:tic_tac_toe/src/features/game/screen/widgets/current_player.dart';
 import 'package:tic_tac_toe/src/features/game/screen/widgets/game_action.dart';
 import 'package:tic_tac_toe/src/features/game/screen/widgets/game_status.dart';
+
+class GameBinding implements Bindings {
+  final bool isMultiplayer;
+
+  GameBinding({required this.isMultiplayer});
+
+  @override
+  void dependencies() {
+    Get.lazyPut<GameController>(() => GameController(isMultiplayer));
+  }
+}
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
@@ -19,6 +35,7 @@ class GameScreen extends StatelessWidget {
           children: [
             GameStatusWidget(txtTheme: txtTheme),
             BoardGameWidget(size: size.width * 0.7),
+            const CurrentPlayer(),
             const GameActionWidget()
           ],
         ),
@@ -27,3 +44,38 @@ class GameScreen extends StatelessWidget {
   }
 }
 
+Future<bool?> showConfirmDialog({
+  required BuildContext context,
+  String title = "Confirm!",
+  required String content,
+}) async {
+  return showDialog<bool>(
+    context: context,
+    builder: (_) => AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      titleTextStyle: const TextStyle(
+        color: Colors.blue,
+        fontSize: 20.0,
+        fontWeight: FontWeight.w600,
+      ),
+      title: Text(title),
+      content: Text(content),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+          child: const Text("Cancel"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(true);
+          },
+          child: const Text("Ok"),
+        ),
+      ],
+    ),
+  );
+}
